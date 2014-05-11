bcddc5142a0606f27627ba9ecf9f84f8d6d690c9
Dinesh137 <dinesh.rockin137@gmail.com>
2014-05-12 04:50:42 +0530

    plot added

---
+++ plot3.R
@@ -0,0 +1,20 @@
+read1<-read.table("C:\\Users\\Dinesh\\Downloads\\computing for data analysis\\4.exploratory data analysis\\week 1\\household_power_consumption.txt",header=TRUE,sep=";")
+read1[,"Date"]<-as.Date(read1[,1],"%d/%m/%Y")
+m<-subset(read1,read1[,1]=="2007-02-01" | read1[,1]=="2007-02-02")
+b<-paste(m$Date,m$Time)
+c<-strptime(b,"%Y-%m-%d %H:%M:%S") ## pasting both together in date_time format
+z<-cbind.data.frame(m,c)
+ m[,"Sub_metering_1"]<-as.numeric(as.character(m[,7]))
+ m[,"Sub_metering_2"]<-as.numeric(as.character(m[,8]))
+ m[,"Sub_metering_9"]<-as.numeric(as.character(m[,9]))
+z<-cbind.data.frame(m,c)
+ sm1<-z[,7]
+ sm2<-z[,8]
+ sm3<-z[,9]
+with(z,plot(z$b,sm1,col="black",type="l",xlab="",ylab="Energy sub metering"))
+with(subset(z,colnames(z)=="Sub_metering_2"),points(z$b,sm2,col="red",type="l"))
+with(subset(z,colnames(z)=="Sub_metering_3"),points(z$b,sm3,col="blue",type="l"))
+legend("topright",lwd=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+dev.copy(png,file="plot3.png")
+dev.off()
